df6871e255ed65be631afeb1e986fffae312db7e
itzVort3x1 <saikaustubh10@gmail.com>
2024-12-15 13:38:10 +0000

    updated the query service and added moderation service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -4,39 +4,44 @@ import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
 
 const PostList = () => {
-  const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState({});
 
-  const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const fetchPosts = async () => {
+        let res;
+        try {
+            res = await axios.get("http://localhost:4002/posts");
+        } catch (err) {
+            console.log(err.message);
+        }
 
-    setPosts(res.data);
-  };
+        setPosts(res?.data || []);
+    };
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+    useEffect(() => {
+        fetchPosts();
+    }, []);
 
-  const renderedPosts = Object.values(posts).map((post) => {
+    const renderedPosts = Object.values(posts).map((post) => {
+        return (
+            <div
+                className="card"
+                style={{ width: "30%", marginBottom: "20px" }}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentList comments={post.comments} />
+                    <CommentCreate postId={post.id} />
+                </div>
+            </div>
+        );
+    });
+
     return (
-      <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
-      >
-        <div className="card-body">
-          <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
-          <CommentCreate postId={post.id} />
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
         </div>
-      </div>
     );
-  });
-
-  return (
-    <div className="d-flex flex-row flex-wrap justify-content-between">
-      {renderedPosts}
-    </div>
-  );
 };
 
 export default PostList;
--- event-bus/index.js
+++ event-bus/index.js
@@ -21,8 +21,15 @@ app.post("/events", (req, res) => {
     axios.post("http://localhost:4002/events", event).catch((err) => {
         console.log(err.message);
     });
+    axios.post("http://localhost:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
